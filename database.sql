2f72318953fae13ebb167f11689aabc5cc65733f
yannnnzz <2024804106@student.uitm.edu.my>
2026-06-21 23:32:23 +0800

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Profile(
+    studentID VARCHAR(20) PRIMARY KEY,
+    name VARCHAR(100),
+    programme VARCHAR(100),
+    email VARCHAR(100),
+    hobbies VARCHAR(255),
+    introduction VARCHAR(500)
+);
